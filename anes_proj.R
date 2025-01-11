7a4e100ccd5691fc3112f44acc6e30aff045306f
“Meeragit config --global user.email “SwaminathanM1999@gmail.com <meera@Bhaskarans-MacBook-Pro.local>
2025-01-11 01:13:30 -0600

    Updated Graphs for Racial A/D no NA

--- anes_proj.R
+++ anes_proj.R
@@ -8,36 +8,39 @@ anes_data <- read_csv(here("data", "anes_pilot_2016.csv"))
 vote2016 <- anes_data$vote16dt
 vote2016 <- ifelse(vote2016 == 9, NA, vote2016)
 vote2016 <- factor(vote2016, 
-                             levels = c(1, 2, 3, 4),
-                             labels = c("Clinton", "Trump", "Someone Else", "Probably Not Voting"))
-
+                   levels = c(1, 2, 3, 4),
+                   labels = c("Clinton", "Trump", "Someone Else", "Probably Not Voting"))
 
 white_adv <- anes_data$wad4b
 white_adv <- ifelse(white_adv == 9, NA, white_adv)
 
-# graphing responses re: white advantage/disadvantage
-# scale: 1 = "large advantage", 4 = "it does not make any difference",
-# 7 = "large disadvantage"
+anes_data$vote2016 <- vote2016
+anes_data$white_adv <- white_adv
 
-ggplot(anes_data, aes(x = factor(white_adv, exclude = NULL))) +
+anes_data_clean <- anes_data %>%
+  filter(!is.na(vote2016) & !is.na(white_adv))
+
+# Graphing responses re: white advantage/disadvantage
+# scale: 1 = "large advantage", 4 = "it does not make any difference", 7 = "large disadvantage"
+ggplot(anes_data_clean, aes(x = factor(white_adv, exclude = NULL))) +
   geom_bar(fill = "darkgreen") +
   labs(
     title = "Distribution of Responses on White Advantage/Disadvantage",
-    x = "Responses (1-7, NA)",
+    x = "Responses",
     y = "Count"
   ) +
   theme_minimal()
 
-ggplot(anes_data, aes(x = factor(white_adv), fill = factor(vote2016))) +
+# Relationship between 2016 Vote and Views on Racial Advantage/Disadvantage
+ggplot(anes_data_clean, aes(x = factor(white_adv), fill = factor(vote2016))) +
   geom_bar() +
   labs(
     title = "Relationship between 2016 Vote and Views on Racial Advantage/Disadvantage",
-    x = "White Adv",
+    x = "Views",
     y = "Count",
     fill = "Vote in 2016"
   ) +
   theme_minimal()
-
 
 warm_do <- anes_data$warmdo
 warm_do <- ifelse(warm_do == 9, NA, warm_do)
